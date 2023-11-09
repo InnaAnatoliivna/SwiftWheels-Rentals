3cf047b0b7466ccdb0445374050b09508175098a
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-11-09 19:40:44 +0100

    init project

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import React, { Suspense, lazy, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SharedLayout from './SharedLayout/SharedLayout';
+import { useDispatch } from 'react-redux';
+import { refreshUser } from 'redux/auth/operations';
+import { useAuth } from 'hooks/useAuth';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
+import Loading from './Loading/Loading';
+
+const HomePage = lazy(() => import('pages/Home/Home'));
+const SignUpPage = lazy(() => import('pages/SignUp/SignUp'));
+const LoginPage = lazy(() => import('pages/Login/Login'));
+const ContactsPage = lazy(() => import('pages/Contacts/Contacts'));
+const ErrorPage = lazy(() => import('pages/ErrorPage/ErrorPage'));
+
+
+export const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+  useEffect(() => {
+    dispatch(refreshUser())
+  }, [dispatch])
+
+  return (
+    !isRefreshing && (
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="register" element={<RestrictedRoute component={<SignUpPage />} redirectTo='/contacts' />} />
+            <Route
+              path="login"
+              element={<RestrictedRoute component={<LoginPage />} redirectTo='/contacts' />} />
+            <Route path="contacts" element={<PrivateRoute component={<ContactsPage />} redirectTo='/' />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    )
+  );
+}
